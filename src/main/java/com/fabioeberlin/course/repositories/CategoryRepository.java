31b109639797ce95432baf0101fbd709d20c0501
Fabio Eberlin <fabioeberlin@gmail.com>
2023-02-15 13:27:02 -0300

    Category

---
+++ src/main/java/com/fabioeberlin/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.fabioeberlin.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fabioeberlin.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
