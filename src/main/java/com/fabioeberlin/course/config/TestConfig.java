73e044008c2f8795458267b4efaf97a5a4f9634d
Fabio Eberlin <fabioeberlin@gmail.com>
2023-02-15 13:48:06 -0300

    Product

--- src/main/java/com/fabioeberlin/course/config/TestConfig.java
+++ src/main/java/com/fabioeberlin/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.fabioeberlin.course.entities.Category;
 import com.fabioeberlin.course.entities.Order;
+import com.fabioeberlin.course.entities.Product;
 import com.fabioeberlin.course.entities.User;
 import com.fabioeberlin.course.entities.enums.OrderStatus;
 import com.fabioeberlin.course.repositories.CategoryRepository;
 import com.fabioeberlin.course.repositories.OrderRepository;
+import com.fabioeberlin.course.repositories.ProductRepository;
 import com.fabioeberlin.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,6 +41,12 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -43,10 +54,14 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 	}
 }
---
+++ src/main/java/com/fabioeberlin/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.fabioeberlin.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fabioeberlin.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
