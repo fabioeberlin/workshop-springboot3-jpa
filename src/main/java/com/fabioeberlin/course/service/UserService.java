6898fb4f60f33158cf5628fdd721e268d02c031a
Fabio Eberlin <fabioeberlin@gmail.com>
2023-02-15 10:20:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/fabioeberlin/course/service/UserService.java
@@ -0,0 +1,26 @@
+package com.fabioeberlin.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabioeberlin.course.entities.User;
+import com.fabioeberlin.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
