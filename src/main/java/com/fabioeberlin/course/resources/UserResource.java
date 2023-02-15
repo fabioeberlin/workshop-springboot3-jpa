8b4a3b3b091da75b1d45275dbb4f6cb65c4951de
Fabio Eberlin <fabioeberlin@gmail.com>
2023-02-15 09:16:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/fabioeberlin/course/resources/UserResource.java
+++ src/main/java/com/fabioeberlin/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999999", "12345");
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 
